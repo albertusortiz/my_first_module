2e2995f5fe18b65eec2fcd88e9d092da9ad89fa2
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-06-13 22:01:54 -0500

    Agregando boton de editar en los registros

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -21,3 +21,26 @@ class MyFirstModule(http.Controller):
     @http.route('/cars/create', auth='public', type='http', website=True)
     def redirect_to_form_car_create(self, **kw):
         return request.render('my_first_module.create_car_form')
+
+    
+    @http.route('/create', auth='public', type='http', website=True)
+    def create_new_car(self, **kw):
+        name = kw.get("name")
+        doors_number = kw.get("doors_number")
+        horse_power = kw.get("horse_power")
+        
+        print("#"*30)
+        print("___create_new_car___")
+        print("kw",kw)
+        print("name:",name)
+        print("doors_number:",doors_number)
+        print("horse_power:",horse_power)
+        print("#"*30)
+        
+        request.env['car.car'].create({
+            'name': name,
+            'doors_number': doors_number,
+            'horse_power': horse_power,
+        })
+        
+        return request.redirect("/cars")
